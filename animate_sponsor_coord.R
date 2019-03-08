4ef9359d46be8152bb34899914cbf4f8b38aa362
Rick Pack <rickeyhp@gmail.com>
2019-03-08 08:26:32 -0500

    AF_sponsor_coord.gif update

--- animate_sponsor_coord.R
+++ animate_sponsor_coord.R
@@ -25,8 +25,9 @@ r_image      <- image_read("R_code_animate.png") %>%
   image_scale(res_use)
 bc_image     <- image_read("BCBSNC.png") %>% 
   image_scale(res_use)
-
-img <- c(plan_image, board_image, board_image, volun_image,
-         lunch_image, p5_image, p7_image, r_image, bc_image)
+# Worked when I had two board_image entries after plan_image
+# failed with board_image start [plan_image no show]
+img <- c(board_image, plan_image, plan_image, volun_image,
+         spons_image, lunch_image, p5_image, p7_image, r_image, bc_image)
 animate2 <- image_animate(img, fps = 0.25)
 image_write(animate2, "AF_sponsor_coord.gif")
