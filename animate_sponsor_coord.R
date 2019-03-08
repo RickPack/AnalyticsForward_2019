16c8dfae69559dae64bec58433515b17f37bd363
Rick Pack <rickeyhp@gmail.com>
2019-03-08 09:16:09 -0500

    added jordan meyer image to gif

--- animate_sponsor_coord.R
+++ animate_sponsor_coord.R
@@ -25,9 +25,11 @@ r_image      <- image_read("R_code_animate.png") %>%
   image_scale(res_use)
 bc_image     <- image_read("BCBSNC.png") %>% 
   image_scale(res_use)
+keynote_image <- image_read("Marketplace_Zillow_JordanMeyer.png") %>% 
+  image_scale(res_use)
 # Worked when I had two board_image entries after plan_image
 # failed with board_image start [plan_image no show]
-img <- c(board_image, plan_image, plan_image, volun_image,
+img <- c(keynote_image, board_image, board_image, plan_image, volun_image,
          spons_image, lunch_image, p5_image, p7_image, r_image, bc_image)
 animate2 <- image_animate(img, fps = 0.25)
 image_write(animate2, "AF_sponsor_coord.gif")
